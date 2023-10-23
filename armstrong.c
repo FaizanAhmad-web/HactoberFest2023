fa7e84ef3a1cbe345707497e9f06493e754fc23f
FaizanAhmad-web <147245219+FaizanAhmad-web@users.noreply.github.com>
2023-10-23 20:59:05 +0530

    armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+// Program for 3 digit Armstrong Number
+
+   /*/  int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+
+        remainder = originalNum % 10;
+        
+       result = ( remainder * remainder * remainder) + result ;
+        
+    
+       originalNum = originalNum/ 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+}*/
+// Program for armstrong number of n digits
+int main() {
+   int num, originalNum, remainder, n = 0;
+   float result = 0.0;
+
+   printf("Enter an integer: ");
+   scanf("%d", &num);
+
+   originalNum = num;
+
+   // store the number of digits of num in n
+   for (originalNum = num; originalNum != 0; ++n) {
+       originalNum /= 10;
+   }
+
+   for (originalNum = num; originalNum != 0; originalNum /= 10) {
+       remainder = originalNum % 10;
+
+      // store the sum of the power of individual digits in result
+      result += pow(remainder, n);
+   }
+
+   // if num is equal to result, the number is an Armstrong number
+   if ((int)result == num)
+    printf("%d is an Armstrong number.", num);
+   else
+    printf("%d is not an Armstrong number.", num);
+   return 0;
+}
